cf8881238fbcee190c225b2c9ed68fd1654546e6
sm924 <smason924@gmail.com>
2020-11-07 19:23:24 +0000

    Updated tests.

--- src/test/java/com/democode/codingchallenge/controllers/CheckSiteControllerTests.java
+++ src/test/java/com/democode/codingchallenge/controllers/CheckSiteControllerTests.java
@@ -47,6 +47,32 @@ class CheckSiteControllerTests {
     }
 
     @Test
+    void testFindByDomain() {
+        new Expectations() {
+            {
+                checkSiteService.getAllSites();
+                result = getSites();
+            }
+        };
+        Site testSite = checkSiteController.getInfo("somedomain", "www.bbc.co.uk");
+        Assert.assertNotNull(testSite.getMalicious());
+        Assert.assertFalse(testSite.getMalicious());
+    }
+
+    @Test
+    void testFindByUrl() {
+        new Expectations() {
+            {
+                checkSiteService.getAllSites();
+                result = getSites();
+            }
+        };
+        Site testSite = checkSiteController.getInfo("malware4u.com", "someUrl");
+        Assert.assertNotNull(testSite.getMalicious());
+        Assert.assertTrue(testSite.getMalicious());
+    }
+
+    @Test
     void testForUnknownSite() {
         new Expectations() {
             {
@@ -64,21 +90,24 @@ class CheckSiteControllerTests {
         Site site1 = new Site();
         site1.setId(1L);
         site1.setDomain("bbc.co.uk");
-        site1.setUrl("http://www.bbc.co.uk/sport/football");
+        site1.setUrl("www.bbc.co.uk");
         site1.setThreat_rating(1);
         site1.setMalicious(false);
+
         Site site2 = new Site();
         site2.setId(2L);
         site2.setDomain("malware4u.com");
-        site2.setUrl("http://malware4u.com/dodgy/");
+        site2.setUrl("www.malware4u.com/dodgy/");
         site2.setThreat_rating(-1);
         site2.setMalicious(true);
+
         Site site3 = new Site();
         site3.setId(3L);
         site3.setDomain("verynaughty.com");
-        site3.setUrl("http://verynaughty.com/its-a-wrongun");
+        site3.setUrl("www.verynaughty.com");
         site3.setThreat_rating(-1);
         site3.setMalicious(true);
+
         sites.add(site1);
         sites.add(site2);
         sites.add(site3);
